68f789ce9dcf5a17e02816cb1234cb1a1b92b965
brunosan <brunosan@gmail.com>
2011-01-18 17:54:55 -0500

    Addign rspec and stuff

--- test/unit/movie_test.rb
+++ test/unit/movie_test.rb
@@ -5,4 +5,12 @@ class MovieTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+
+  test 'Movie attributes must not be empty' do
+    movie = Movie.new
+    assert movie.invalid?
+    assert movie.errors[:title].any?
+    assert movie.errors[:pal].any?
+  end
+  
 end
