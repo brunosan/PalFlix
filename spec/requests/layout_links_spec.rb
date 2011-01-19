2396190f62c263b0d63df3b5a4e07884051f8ffb
brunosan <brunosan@gmail.com>
2011-01-18 20:20:03 -0500

    live with changes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -4,7 +4,7 @@ describe "LayoutLinks" do
 
   it "should have a Home page at '/'" do
     get '/'
-    response.should have_selector('title', :content => "Home")
+    response.should have_selector('title', :content => "PalFlix | Home")
   end
 
   it "should have a Contact page at '/contact'" do
